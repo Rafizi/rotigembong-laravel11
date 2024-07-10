fe7feffd1993b5858fbba673c80c3df83aca6ba1
Rafizi <arrafizinaufal@gmail.com>
2024-07-10 11:24:47 +0700

    first commit

---
+++ resources/views/frontend/pages/about-us.blade.php
@@ -0,0 +1,101 @@
+@extends('frontend.layouts.master')
+
+@section('title','E-SHOP || About Us')
+
+@section('main-content')
+
+	<!-- Breadcrumbs -->
+	<div class="breadcrumbs">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="bread-inner">
+						<ul class="bread-list">
+							<li><a href="index1.html">Home<i class="ti-arrow-right"></i></a></li>
+							<li class="active"><a href="blog-single.html">About Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Breadcrumbs -->
+
+	<!-- About Us -->
+	<section class="about-us section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-12">
+						<div class="about-content">
+							@php
+								$settings=DB::table('settings')->get();
+							@endphp
+							<h3>Welcome To <span>Roti Gembong Raja Rasa</span></h3>
+							<p>@foreach($settings as $data) {{$data->description}} @endforeach</p>
+							<div class="button">
+								<a href="{{route('blog')}}" class="btn">Our Blog</a>
+								<a href="{{route('contact')}}" class="btn primary">Contact Us</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-12">
+						<div class="about-img overlay">
+							{{-- <div class="button">
+								<a href="https://www.youtube.com/watch?v=nh2aYrGMrIE" class="video video-popup mfp-iframe"><i class="fa fa-play"></i></a>
+							</div> --}}
+							<img src="@foreach($settings as $data) {{$data->photo}} @endforeach" alt="@foreach($settings as $data) {{$data->photo}} @endforeach">
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+	<!-- End About Us -->
+
+
+	<!-- Start Shop Services Area -->
+	<section class="shop-services section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-rocket"></i>
+						<h4>Gratis Ongkir</h4>
+						<p>Gratis ongkir dan pengiriman order cepat</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-reload"></i>
+						<h4>Jaminan Pengganti</h4>
+						<p>Tidak enak atau tidak sesuai pesanan, jaminan pengganti</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-lock"></i>
+						<h4>Pembayaran Aman</h4>
+						<p>100% Checkout Aman</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-tag"></i>
+						<h4>Harga Terbaik</h4>
+						<p>Harga Murah dan Terjangkau</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Shop Services Area -->
+
+	@include('frontend.layouts.newsletter')
+@endsection
